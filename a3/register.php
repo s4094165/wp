120e47c9e7139d8d5d3bbd986aeb3dcc2c973ff8
s4094165 <s4094165@students.rmit.edu.au>
2024-10-31 09:47:50 +1100

    Modified register.php

--- a3/register.php
+++ a3/register.php
@@ -4,6 +4,11 @@ $title = "RegisterUser";
 
 include_once('includes/header.inc');
 include_once('includes/nav.inc');
+
+$date = new DateTime('now', new DateTimeZone('Australia/Melbourne'));
+$cd   = $date->format('Y-m-d');
+$time = $date->format('h:i:s A');
+$date_format = explode('-', $cd);
 ?>
 <div class="content-wrapper ">
    <div class="content ">
@@ -12,14 +17,31 @@ include_once('includes/nav.inc');
 	    	<h2>Register User</h2>
 	    	<p>You can Register as new User here</p>
 		</header>
-		<form action="reg.php" method="post">
+		<?php
+		if(isset($_SESSION['error'])){?>
+		<div class="alert alert-danger alert-dismissible fade show" role="alert">
+			<?php echo $_SESSION['error']; unset($_SESSION['error']);?>
+			<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+		</div>
+		<?php } 
+		if(isset($_SESSION['success'])){?>
+			<div class="alert alert-success alert-dismissible fade show" role="alert">
+			<?php echo $_SESSION['success']; unset($_SESSION['success']);?>
+			<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+		</div>
+		<?php } ?>
+		<form action="users.php" method="post">
 	          <div class="form-group">
 	            <label for="UserName " class="required">User Name </label>
-	            <input type="text" class="form-control" id="UserName " placeholder="Enter User Name " required="">
+	            <input type="text" class="form-control" id="UserName " placeholder="Enter User Name " required name="UserName">
 	          </div>
 	          <div class="form-group">
 	            <label for="Password" class="required">Password</label>
-	            <input type="password" class="form-control" id="Password" placeholder="Password" required="">
+	            <input type="password" class="form-control" id="Password" placeholder="Password" required name="Password">
+	          </div>
+	          <div class="form-group">
+	            <label for="date" class="required">Reg Date</label>
+	            <input type="datetime-local" class="form-control" id="date" placeholder="Date" required name="reg_date">
 	          </div>
 	          <br>
 	        <div class="button-container">
